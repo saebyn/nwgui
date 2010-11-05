a8f53a727aa435a551bf453ab98e075f3a807ba0
John Weaver <john@pledge4code.com>
2010-11-04 20:53:49 -0700

    Basic distutils file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+distutils setup.py file.
+
+"""
+
+from distutils.core import setup
+
+setup(
+    name = 'nwgui',
+    version = '0.1',
+    description = 'A simple GUI package for pygame.',
+
+    author = 'John Weaver',
+    author_email = 'john@pledge4code.com',
+)
+
